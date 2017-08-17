ab96c0cdb38569f47436fdb85b4afe42608ebe09
Lindsay Bloom <lindsay.bloom@tufts.edu>
2017-08-17 13:29:17 -0400

    fixed get errors

--- public/signup.js
+++ public/signup.js
@@ -12,8 +12,48 @@ firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error
     // ...
 });
 
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+
 firebase.auth().signOut().then(function() {
     // Sign-out successful.
 }).catch(function(error) {
     // An error happened.
+});
+
+//Sign in with Google
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+firebase.auth().signInWithRedirect(provider);
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
 });
